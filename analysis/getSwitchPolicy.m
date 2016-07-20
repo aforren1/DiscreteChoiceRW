3954805872b816e3306e4e49f155907e463e3596
Adrian Haith <adrianhaith@gmail.com>
2016-07-20 16:52:33 -0400

    added code to analyze switch/stay policy

---
+++ analysis/getSwitchPolicy.m
@@ -0,0 +1,37 @@
+function [p_stay1 p_stay2 N_stay1 N_stay2] = getSwitchPolicy(u,reward)
+% figures out first and second-order switching policies
+%
+% inputs:   u - sequence of actions
+%           reward - sequence of associated rewards
+%
+% outputs:  p_stay1 - first order policy p(stay|[win lose]);
+%           p_stay2 - second order policy:
+%                       p(stay|i,j,k), i = win on previous trial
+%                                      j = stay after prev trial
+%                                      k = win on current trial
+%
+%           N_stay1, N_Stay2 - Number of trials contributing to policy estimates
+%           
+
+%% second-order policy
+win = reward(2:end-1);
+win_prev = reward(1:end-2);
+stay = (u(2:end-1)==u(3:end));
+stay_prev = (u(1:end-2)==u(2:end-1));
+
+B = [0 1];
+
+for i = 1:2
+    for j = 1:2
+        for k = 1:2
+            % find all trials that meet this criterion
+            i_meet{i,j,k} = find(win_prev==B(i) & stay_prev==B(j) & win==B(k));
+            p_stay2(i,j,k) = mean(stay(i_meet{i,j,k}));
+            N_stay2(i,j,k) = length(i_meet{i,j,k});
+        end
+    end
+    i_win{i} = find(win==B(i));
+    p_stay1(i) = mean(stay(i_win{i}));
+    N_stay1(i) = length(i_win{i});
+end
+
---
+++ analysis/loadData.m
@@ -0,0 +1,38 @@
+% analyze choice behavior in terms of stay|win, stay|lose (1st order) and
+% second order analog (e.g. stay|win-stay-win).
+data = dlmread('../data/id1_block2_nchoice4.csv',',',1,0) %load data
+
+action = data(:,3); % which actions the subject took
+reward = data(:,5); % whether or not a reward was earned
+
+[p_1 p_2 N_1 N_2] = getSwitchPolicy(action,reward); % extract the policies
+
+% visualize second-order policy
+% NB - need to use 'Rotate 3D' tool to view these properly
+figure(1); clf; hold on
+subplot(2,2,1); hold on
+title('p(stay) | lose')
+bar3(p_2(:,:,1))
+xlabel('1=lose, 2=win')
+ylabel('1=switch, 2=stay')
+axis([.5 2.5 .5 2.5 0 1])
+
+subplot(2,2,2); hold on
+title('p(stay) | win')
+bar3(p_2(:,:,2))
+
+xlabel('1=lose, 2=win')
+ylabel('1=switch, 2=stay')
+axis([.5 2.5 .5 2.5 0 1])
+
+subplot(2,2,3); hold on
+title('Number of trials of each type')
+bar3(N_2(:,:,1))
+xlabel('1=lose, 2=win')
+ylabel('1=switch, 2=stay')
+
+subplot(2,2,4); hold on
+title('Number of trials of each type')
+bar3(N_2(:,:,2))
+xlabel('1=lose, 2=win')
+ylabel('1=switch, 2=stay')
