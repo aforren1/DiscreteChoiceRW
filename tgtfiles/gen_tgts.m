367d425b6904f9065f4c70bcda38bf15b63a89aa
Alex Forrence <aforren1@jhu.edu>
2016-07-20 15:45:19 -0400

    Adrian's data

--- tgtfiles/gen_tgts.m
+++ tgtfiles/gen_tgts.m
@@ -1,13 +1,13 @@
 
-num_choices = 6;
+num_choices = 8;
 scale = 0.075;
-num_trials = 10;
-lower = 0.1;
-upper = 0.9;
-num_blocks = 1;
+num_trials = 200;
+lower = 0.25;
+upper = 0.75;
+num_blocks = 4 ;
 
 for ii = 1:num_blocks
-    rand('seed', ii*num_choices);
+    rand('seed', 100*ii*num_choices);
     output = DiscreteRandomWalk(num_choices, scale, num_trials, lower, upper);
     csvwrite(['tgtfiles/', 'block', num2str(ii), '_nchoice', num2str(num_choices), '.csv'], output);
 end
